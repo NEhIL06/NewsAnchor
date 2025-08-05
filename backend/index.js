f02fbd6cd4b6a37e64867cd5fe97ea534418ec62
Nehil <chnadrakarnehil.06112002@gmail.com>
2025-08-05 17:39:43 +0530

    update on the backend index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import { exec } from "child_process";
 import cors from "cors";
 import dotenv from "dotenv";
 import voice from "elevenlabs-node";
-import express from "express";
+import express, { text } from "express";
 import OpenAI from "openai";
 import axios from "axios";
 dotenv.config();
@@ -11,11 +11,29 @@ import {GoogleGenerativeAI,HarmCategory,HarmBlockThreshold } from "@google/gener
 import fs,{promises} from "fs";
 
 import { getJson } from "serpapi";
+import { Interface } from "readline";
+import { types } from "util";
 
 
 const client = new ElevenLabsClient({
   apiKey: process.env.ELEVEN_LABS_API_KEY
 });
+
+const ticket = {
+  'ticketNumber': '',
+  'source station': '',
+  'destination station': '',
+  'departure time': '',
+  'arrival time': '',
+  'seat Number': '',
+  'passenger name': '',
+  'passenger age': '',
+  'passenger gender': '',
+  'passenger class': '',
+  'passenger status': '',
+  'passenger nationality': '',
+  'passenger ticket number': '',
+}
 
 
 // const openai = new OpenAI({
@@ -27,7 +45,7 @@ const FLASK_SERVER_URL = "http://127.0.0.1:5000/generate-speech";
 
 const elevenLabsApiKey = process.env.ELEVEN_LABS_API_KEY;
 const voiceID = "MF3mGyEYCl7XYWbV9V6O";
-
+const geminiAPIKey = process.env.GEMINI_API_KEY;
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -50,6 +68,30 @@ const execCommand = (command) => {
   });
 };
 
+const speechToText = async (textInput,fileName) => {
+  try {
+    const flaskResponse = await axios.post(
+      FLASK_SERVER_URL,
+      {
+        text: textInput,
+        filename: fileName,
+      },
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+
+    if (flaskResponse.status === 200) {
+      console.log(`Speech generated successfully for message ${i}`);
+      message.audioFilePath = flaskResponse.data.file_path;
+    } else {
+      console.error(`Error generating speech for message ${i}`);
+    }
+  } catch (error) {
+    console.error("Error calling Flask API:", error.message);
+  }
+}
+
 const lipSyncMessage = async (message) => {
   const time = new Date().getTime();
   console.log(`Starting conversion for message ${message}`);
@@ -68,18 +110,18 @@ const lipSyncMessage = async (message) => {
 app.post("/chat", async (req, res) => {
   
 
-  const userMessage = req.body.message;
-  const genAI = new GoogleGenerativeAI("");
+  const userMessage = req.body.message; // The Request recieved from the user to process
+  const genAI = new GoogleGenerativeAI(""); 
 
-  // Initialize the generative model with the specified model ID
+  
   // const model = genAI.getGenerativeModel({
   //   model: "gemini-2.0-flash-exp",
   // });
 
-  // Set the system instruction during model initialization
+ 
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
-    systemInstruction: "You Are a AI news Anchor",
+    systemInstruction: "Your are Avanti and AI ticket booking assistant",
   });
   // const generationConfig = {
   //   temperature: 1,
@@ -96,15 +138,6 @@ app.post("/chat", async (req, res) => {
     text = text.replace(/```/g, ""); // Remove closing markers (global replace)
     return text.trim(); // Remove leading/trailing whitespace
   }
-  const searchPrompt = `Find the latest and most detailed news articles on "${userMessage}". Prioritize sources that provide summaries, insights, and expert analysis rather than just headlines.`;
-
-
-  const NewsPrompt = `Generate a Search Query for Google News based on the request recieved by the User ${userMessage}  `;
-  // const chatSession = model.startChat({
-  //   generationConfig,
-  //   history: [
-  //   ],
-  // });
 
   const chat = model.startChat({
     history: [
@@ -119,57 +152,29 @@ app.post("/chat", async (req, res) => {
     ],
   });
   
-  //let result = await chat.sendMessageStream("I have 2 dogs in my house.");
-  // for await (const chunk of result.stream) {
-  //   const chunkText = chunk.text();
-  //   process.stdout.write(chunkText);
+  // const checktype = `Tell me what type of Request does the user want to make based on the following message ${userMessage}
+  //   You have to reply in only one word witht the type of request.
+  //   The possible types are:
+  //   1. Ticket Booking
+  //   2. Bank Operations
+  // `;
+
+  const prompt = `Extract the information for the ticket booking from userMessage: ${userMessage} to fill the data in ticket: ${ticket} you have to look for Name 
+    passenger name,passenger age,passenger gender,source station and destination station,departure time from the source staation 
+    and return the data in the format of JSON structure of ticket put the data required same as the data in userMessage and for the rest generate some REALISTIC data if all data the needs to be there i.e
+    passenger name,passenger age,passenger gender,source station and destination station,departure time is not provided return "False"`
+
+  // if( type === "Ticket Booking"){
+  //   prompt = `Extract the information for the ticket booking from ${userMessage} to fill the data in ticket format: ${ticket} you have to look for Name 
+  //   passenger name,passenger age,passenger gender,source station and destination station,departure time from the source staation 
+  //   and return the data in the format of JSON structure of ticket put the data required same as the  if all data is not provided return "False"`
   // }
-  
-  const fetchNews = async (userMessage) => {
-    return new Promise((resolve, reject) => {
-        getJson(
-            {
-                engine: "google_news",
-                q: `"${userMessage}"`, // Ensures exact match
-                gl: "in",
-                hl: "en",
-                num: 10, // Increase results
-                sort_by: "date", // Sort by latest news
-                api_key: ""
-            },
-            (json) => {
-                 // Debugging
-                if (json && json.news_results && json.news_results.length > 0) {
-                  const headlines = json.news_results.slice(0, 10).map(news => news.title); // Extracting top 10 headlines relevent to the search
-                  resolve(headlines);
-                } else {
-                    reject(new Error("No relevant news found for " + userMessage));
-                }
-            }
-        );
-    });
-};
-  
-  // Usage Example
-  const News = await fetchNews(userMessage);
-  //fs.writeFileSync("news.json", JSON.stringify(News, null, 2), "utf-8"); // to save the Titles on Json File
-  
-  // Create the prompt for the model
-  const prompt = `
+  // else if( type === "Bank Operations"){
+  //   prompt = ``
+  // }
 
-  You are an intelligent news assistant. Based on the given news articles retrieved from the Google News API:
-
-  ### Instructions:  
-  - Analyze the provided news titles and descriptions.  
-  - Identify the topics or themes that appear most relevant or popular.  
-  - Select the 3 distinct most compelling headlines that best match user interest.  
-  - Ensure the headlines are *concise, engaging, and informative*.  
-  - Do *not* include any extra text, explanations, or formatting.  
-  - Output *only a JSON array* with 3 headline objects.
-
-  News API data = ${News}
-  
-  Your task is to structure the selected News Headlines(No changes is needed in the Headlines) in the Following manner.  
+/**
+ * Your task is to structure the selected News Headlines(No changes is needed in the Headlines) in the Following manner.  
   You must respond with only a JSON array of up to 3 news messages. 
   Each message must be an object with the following properties relevant to that news headline:
   - *Text*: (string) containing the news headline.
@@ -184,57 +189,61 @@ app.post("/chat", async (req, res) => {
       "animation": "Talking_0"
     }
   ]
-`;
+ */
 
-//   // To query /v2/everything
-//   // You must include at least one q, source, or domain
-    
-    const result = await chat.sendMessage(prompt);
-  //  console.log(result.response.text);
-//   // Generate content using the model
-//    // const test = await model.generateContent(prompt);
-//     //checking the type of unforamatted
-//     // Check if the completion contains valid choices
-    const completion = removeCodeBlockMarkers(result.response.candidates[0].content.parts[0].text)
-    let messages = JSON.parse(completion);
-   // console.log(messages); // Log the parsed messages
+  const result = await chat.sendMessage(prompt);
 
-    
-    if (messages.messages) {
-      console.log(messages.message);//Checking whether output is coming out or not
-      messages = messages.messages; 
+  const ticketJson = removeCodeBlockMarkers(result.response.candidates[0].content.parts[0].text); 
+  console.log(ticketJson);
+  
+  if(ticketJson === "False"){
+    textInput = "Please provide all your information your name , age , gender , source station and destination station and departure time"
+    await speechToText(textInput,`audios/message_${0}.mp3`);
+    messages =[{
+      messages: textInput,
+      facialExpression: "default",
+      animation: "Idle",
+    }]
+    res.send({ messages });
+    return;
+  }
+  prompt = `Now give me a message to send to the user for the comfirmation of the ticket booking based on the data you have extracted from the message ${userMessage} and the ticket data ${JSON.stringify(ticket)}
+    Your task is to structure a one liner message to send the user for the confirmation of the ticket booking based on the data you have extracted from the message ${userMessage} and the ticket data ${JSON.stringify(ticket)}
+  message must be an object with the following properties relevant to that news headline:
+  - *Text*: (string) containing the confirmation message.
+  - *facialExpression*: (string, one of: 'default') most relevant to the news from an anchor's perspective.
+  - *animation*: (string, one of:'Idle') most relevant to the news.
+
+  Crucially: Do not include any surrounding text, code blocks, or explanations. Return only the raw JSON data. Here is an example of how your response should look:
+  [
+    {
+      "text": " message here.",
+      "facialExpression": "default",
+      "animation": "idleStanding"
     }
-    for (let i = 0; i < messages.length; i++) {
+  ]
+  `
+  result = await chat.sendMessage(prompt);
+  const temp = removeCodeBlockMarkers(result.response.candidates[0].content.parts[0].text);
+  let messages = JSON.parse(temp);
+  
+
+  if (messages.messages) {
+    console.log(messages.message);
+    messages = messages.messages; 
+  }
+    
+  for (let i = 0; i < messages.length; i++) {
       const message = messages[i];
       const fileName = `audios/message_${i}.mp3`;
       const textInput = message.text; // The text you wish to convert to speech
       //Post 
-      try {
-        const flaskResponse = await axios.post(
-          FLASK_SERVER_URL,
-          {
-            text: textInput,
-            filename: fileName,
-          },
-          {
-            headers: { "Content-Type": "application/json" },
-          }
-        );
-
-        if (flaskResponse.status === 200) {
-          console.log(`Speech generated successfully for message ${i}`);
-          message.audioFilePath = flaskResponse.data.file_path;
-        } else {
-          console.error(`Error generating speech for message ${i}`);
-        }
-      } catch (error) {
-        console.error("Error calling Flask API:", error.message);
-      }
+      await speechToText(textInput,fileName);
       
       await lipSyncMessage(i);
       message.audio = await audioFileToBase64(fileName);
       message.lipsync = await readJsonTranscript(`audios/message_${i}.json`);
-    }
+  }
 
     res.send({ messages });
 });
@@ -278,3 +287,33 @@ app.listen(port, () => {
   console.log(`NewsAnchor listening on port ${port}`);
 });
 
+//   const fetchNews = async (userMessage) => {
+//     return new Promise((resolve, reject) => {
+//         getJson(
+//             {
+//                 engine: "google_news",
+//                 q: `"${userMessage}"`, // Ensures exact match
+//                 gl: "in",
+//                 hl: "en",
+//                 num: 10, // Increase results
+//                 sort_by: "date", // Sort by latest news
+//                 api_key: ""
+//             },
+//             (json) => {
+//                  // Debugging
+//                 if (json && json.news_results && json.news_results.length > 0) {
+//                   const headlines = json.news_results.slice(0, 10).map(news => news.title); // Extracting top 10 headlines relevent to the search
+//                   resolve(headlines);
+//                 } else {
+//                     reject(new Error("No relevant news found for " + userMessage));
+//                 }
+//             }
+//         );
+//     });
+// };
+  
+  
+  //const News = await fetchNews(userMessage);
+  //fs.writeFileSync("news.json", JSON.stringify(News, null, 2), "utf-8"); // to save the Titles on Json File
+  
+  // Create the prompt for the model
